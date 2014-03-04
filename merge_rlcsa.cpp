c96f5aa0d69d824e662ff4edb09f732b3fe94086
Adam Novak <anovak@soe.ucsc.edu>
2014-03-03 17:12:56 -0800

    Adding a multi-merge function to merge_rlcsa

--- merge_rlcsa.cpp
+++ merge_rlcsa.cpp
@@ -26,23 +26,41 @@ main(int argc, char** argv)
   std::cout << "RLCSA merger" << std::endl;
   if(argc < 3)
   {
-    std::cout << "Usage: merge_rlcsa original additional [threads]" << std::endl;
+    std::cout << "Usage: merge_rlcsa [-threads] original additional" << std::endl;
     return 1;
   }
 
-  int original_parameter = 1, additional_parameter = 2, threads_parameter = 3;
+  int original_parameter = 2, additional_parameter = 3, threads_parameter = 1;
 
-  std::string additional_name = argv[additional_parameter];
-  std::cout << "Index to add: " << additional_name << std::endl;
+  usint threads = 1;
+  if(argc > threads_parameter && argv[threads_parameter][0] == '-')
+  {
+    threads = std::min(MAX_THREADS, std::max(atoi(argv[threads_parameter] + 1), 1));
+  }
+  else 
+  {
+    original_parameter--;
+    additional_parameter--;
+  }
+
+  if(argc < additional_parameter)
+  {
+    std::cerr << "Error: specify the index to merge into and one index to add." << std::endl;
+    return 1;
+  }
 
   std::string base_name = argv[original_parameter];
   std::cout << "Index to update: " << base_name << std::endl;
-
-  usint threads = 1;
-  if(argc > threads_parameter)
+  
+  std::vector<std::string> additional_names;
+  
+  for(int i = additional_parameter; i < argc; i++)
   {
-    threads = std::min(MAX_THREADS, std::max(atoi(argv[threads_parameter]), 1));
+    std::string additional_name = argv[i];
+    std::cout << "Index to add: " << additional_name << std::endl;
+    additional_names.push_back(additional_name);
   }
+  
   std::cout << "Threads: " << threads << std::endl; 
   std::cout << std::endl;
 
@@ -68,10 +86,13 @@ main(int argc, char** argv)
   RLCSABuilder builder(parameters.get(RLCSA_BLOCK_SIZE), parameters.get(SAMPLE_RATE), 0, threads, originalIndex);
   std::cout << " (" << (readTimer() - mark) << " seconds)" << std::endl;
   
-  mark = readTimer();
-  std::cout << "Increment: " << additional_name; std::cout.flush();
-  builder.insertFromFile(additional_name);
-  std::cout << " (" << (readTimer() - mark) << " seconds)" << std::endl;
+  for(std::vector<std::string>::iterator i = additional_names.begin(); i != additional_names.end(); ++i)
+  {
+    mark = readTimer();
+    std::cout << "Increment: " << *i; std::cout.flush();
+    builder.insertFromFile(*i);
+    std::cout << " (" << (readTimer() - mark) << " seconds)" << std::endl;
+  }
   
   std::cout << std::endl;
   megabytes = getRLCSA(builder, base_name);
